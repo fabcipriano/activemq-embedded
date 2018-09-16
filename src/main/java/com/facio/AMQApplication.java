57edda3b382e1e7c49d1b114b14a2ec416d8b339
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2018-09-15 23:39:19 -0300

    Initial version

---
+++ src/main/java/com/facio/AMQApplication.java
@@ -0,0 +1,12 @@
+package com.facio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMQApplication.class, args);
+	}
+}
---
+++ src/main/java/com/facio/config/JmsConfig.java
@@ -0,0 +1,14 @@
+package com.facio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+/**
+ *
+ * @author fabiano
+ */
+@Configuration
+@EnableJms
+public class JmsConfig {
+    
+}
